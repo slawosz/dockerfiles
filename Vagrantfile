11834f78d6713a8d729eba34e7bc0c0f921ce9f1
Sławosz <slawosz@gmail.com>
2014-03-25 22:36:21 +0100

    rvm and ruby-2.0.0 Dockerfiles

--- Vagrantfile
+++ Vagrantfile
@@ -10,6 +10,7 @@ Vagrant.configure("2") do |config|
   config.vm.provider :virtualbox do |vb, override|
     vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+    vb.memory = 1024
   end
 
   config.vm.provision "docker" do |d|
--- build.sh
+++ build.sh
@@ -1,31 +1,13 @@
 #/bin/sh
 
-dirs=()
-
-i=1
-
-for dir in */; do
-  dirname=$(echo $dir | sed 's/\/$//')
-  dirs+=($dirname)
-  echo "($((i++))) $dirname"
-done
-
-echo "Which project you want to build"
-echo -n "> "
-
-read anwser
-
-echo "dirs: $dirs"
-IMAGE_NAME=$dirs($anwser)
-
-echo $IMAGE_NAME
+IMAGE_NAME=$1
 
 if [ -z $REGISTRY_URL ]; then
   echo "Provide REGISTRY_URL to continue"
   exit 1
 fi
 
-IMAGE=$REGISTRY_URL$IMAGE_NAME
+IMAGE=$REGISTRY_URL/$IMAGE_NAME
 
 echo "Building $IMAGE..."
 docker build -t $IMAGE $IMAGE_NAME
